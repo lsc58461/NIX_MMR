225a5987f1e3f376f7e834db3313c9336867e2ae
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2022-06-04 02:41:11 +0900

    Update NIX_MMR.py

--- NIX_MMR.py
+++ NIX_MMR.py
@@ -15,8 +15,8 @@ class MMR_Check(commands.Cog):
                 .add_field(name='솔로랭크', value='```css\n{}\n```'.format(_Rank[0]), inline = False)
                 .add_field(name='노말', value='```css\n{}\n```'.format(_Normal[0]), inline = False)
                 .add_field(name='무작위 총력전', value='```css\n{}\n```'.format(_ARAM[0]), inline = False)
-                .set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url))
-                .set_footer(text='API BY whatismymmr.com')
+                .set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
+                .set_footer(text='API BY whatismymmr.com'))
                 
             await ctx.send(embed=embed)
             await ctx.send('현재 API 문제로 인해 사용이 불가능합니다.\n빠른 해결을 위해 노력하겠습니다.')
