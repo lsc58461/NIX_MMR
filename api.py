963e93d0ca828af2c18029e55bf6692423b610c3
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2023-07-19 16:59:08 +0900

    Update api.py

--- api.py
+++ api.py
@@ -4,114 +4,64 @@ This code uses the api on the site above to retrieve the mmr of the League of Le
 
 '''
 
-
+import time
+from Now_Time import Time
+import json
 import requests
-import os
+from concurrent.futures import ThreadPoolExecutor
 
-
-URL = 'https://kr.whatismymmr.com/api/v1/summoner?name='
 API_KEY = os.environ['API_KEY']
-def Rank(search='hide on bush'):
+URL = 'https://kr.whatismymmr.com/api/v1/summoner?name={}&apiKey={}'
+
+def get_summoner_data(search, game_type):
+    print(f'{Time()}MMR_API.py_Game_Type : {game_type}\n')
+    start = time.time()
+    url = URL.format(search, API_Key)
     try:
-        Read_Json = requests.get(f"{URL}{search}&apiKey={API_KEY}").json()
-        print(Read_Json)
-        if 'error' in Read_Json:
-            if Read_Json['error']['code'] == 0:
-                return '예기치 않은 내부 서버 오류입니다.', None
-            elif Read_Json['error']['code'] == 1:
-                return '데이터베이스에 연결할 수 없습니다.', None
-            elif Read_Json['error']['code'] == 100:
-                return '소환사는 기록에 없습니다.', None
-            elif Read_Json['error']['code'] == 101:
-                return '소환사에 대한 최근 MMR 데이터가 없습니다.', None
-            elif Read_Json['error']['code'] == 200:
-                return '"이름" 쿼리 매개변수가 없습니다.', None
-            elif Read_Json['error']['code'] == 9001:
-                return '요청이 너무 많습니다.', None
-                    
-        _avg = Read_Json['ranked']['avg']
-        _err = Read_Json['ranked']['err']
-        _warn = Read_Json['ranked']['warn']
-        _closestRank = Read_Json['ranked']['closestRank']
-        _percentile = Read_Json['ranked']['percentile']
-        if _avg == None:
-            print(_avg, _err, _warn, _closestRank, _percentile)
+        read_json = requests.get(url).json()
+        error = read_json.get('error')
+        if error:
+            error_code = error.get('code')
+            error_msgs = {
+                0: '예기치 않은 내부 서버 오류입니다.',
+                1: '데이터베이스에 연결할 수 없습니다.',
+                100: '소환사는 기록에 없습니다.',
+                101: '소환사에 대한 최근 MMR 데이터가 없습니다.',
+                200: '"이름" 쿼리 매개변수가 없습니다.',
+                9001: '요청이 너무 많습니다.',
+            }
+            error_msg = error_msgs.get(error_code, '알 수 없는 오류가 발생했습니다.')
+            print(f'{Time()}MMR_API.py_Error : {error_msg}\n')
+            return error_msg, None
+
+        mmr_data = read_json.get(game_type, None)
+        if mmr_data is None:
             return '데이터가 충분하지 않습니다.', None
-        if _percentile < 50:
-                _percentile = '하위 ' + str(_percentile)
-        else:
-            _percentile = '상위 ' + str(_percentile)
-        print(_avg, _err, _warn, _closestRank, _percentile)
-        return f'[MMR]\n{_avg}±{_err}\n\n{_closestRank}의 {_percentile}%의 소환사들과 비슷합니다.', f'{_avg}±{_err}'
+
+        avg = mmr_data.get('avg', None)
+        err = mmr_data.get('err', None)
+        closest_rank = mmr_data.get('closestRank', None)
+        percentile = mmr_data.get('percentile', None)
+        if avg is None:
+            return '데이터가 충분하지 않습니다.', None
+
+        percentile_text = '하위' if percentile < 50 else '상위'
+        result_str = f'[MMR] {avg}±{err}\n\n{closest_rank}의 {percentile_text} {percentile}%의 소환사들과 비슷합니다.'
+        result_val = f'{avg}±{err}'
+        print(f'{Time()}MMR_API.py_time : {time.time() - start}\n')
+        return result_str, result_val
+
     except:
-        return '잠시 후 다시시도 해주세요.\n문제가 계속 된다면 문의 바랍니다.', None
+        return '잠시 후 다시 시도해주세요.\n문제가 계속된다면 문의 바랍니다.', None
+
+def Rank(search='hide on bush'):
+    with ThreadPoolExecutor() as executor:
+        return executor.submit(get_summoner_data, search, 'ranked').result()
 
 def Normal(search='hide on bush'):
-    try:
-        Read_Json = requests.get(f"{URL}{search}&apiKey={API_KEY}").json()
-        print(Read_Json)
-        if 'error' in Read_Json:
-            if Read_Json['error']['code'] == 0:
-                return '예기치 않은 내부 서버 오류입니다.', None
-            elif Read_Json['error']['code'] == 1:
-                return '데이터베이스에 연결할 수 없습니다.', None
-            elif Read_Json['error']['code'] == 100:
-                return '소환사는 기록에 없습니다.', None
-            elif Read_Json['error']['code'] == 101:
-                return '소환사에 대한 최근 MMR 데이터가 없습니다.', None
-            elif Read_Json['error']['code'] == 200:
-                return '"이름" 쿼리 매개변수가 없습니다.', None
-            elif Read_Json['error']['code'] == 9001:
-                return '요청이 너무 많습니다.', None
-
-        _avg = Read_Json['normal']['avg']
-        _err = Read_Json['normal']['err']
-        _warn = Read_Json['normal']['warn']
-        _closestRank = Read_Json['normal']['closestRank']
-        _percentile = Read_Json['normal']['percentile']
-        if _avg == None:
-            print(_avg, _err, _warn, _closestRank, _percentile)
-            return '데이터가 충분하지 않습니다.', None
-        if _percentile < 50:
-                _percentile = '하위 ' + str(_percentile)
-        else:
-            _percentile = '상위 ' + str(_percentile)
-        print(_avg, _err, _warn, _closestRank, _percentile)
-        return f'[MMR]\n{_avg}±{_err}\n\n{_closestRank}의 {_percentile}%의 소환사들과 비슷합니다.', f'{_avg}±{_err}'
-    except:
-        return '잠시 후 다시시도 해주세요.\n문제가 계속 된다면 문의 바랍니다.', None
+    with ThreadPoolExecutor() as executor:
+        return executor.submit(get_summoner_data, search, 'normal').result()
 
 def ARAM(search='hide on bush'):
-    try:
-        Read_Json = requests.get(f"{URL}{search}&apiKey={API_KEY}").json()
-        print(Read_Json)
-        if 'error' in Read_Json:
-            if Read_Json['error']['code'] == 0:
-                return '예기치 않은 내부 서버 오류입니다.', None
-            elif Read_Json['error']['code'] == 1:
-                return '데이터베이스에 연결할 수 없습니다.', None
-            elif Read_Json['error']['code'] == 100:
-                return '소환사는 기록에 없습니다.', None
-            elif Read_Json['error']['code'] == 101:
-                return '소환사에 대한 최근 MMR 데이터가 없습니다.', None
-            elif Read_Json['error']['code'] == 200:
-                return '"이름" 쿼리 매개변수가 없습니다.', None
-            elif Read_Json['error']['code'] == 9001:
-                return '요청이 너무 많습니다.', None
-                
-        _avg = Read_Json['ARAM']['avg']
-        _err = Read_Json['ARAM']['err']
-        _warn = Read_Json['ARAM']['warn']
-        _closestRank = Read_Json['ARAM']['closestRank']
-        _percentile = Read_Json['ARAM']['percentile']
-        if _avg == None:
-            print(_avg, _err, _warn, _closestRank, _percentile)
-            return '데이터가 충분하지 않습니다.', None
-        if _percentile < 50:
-                _percentile = '하위 ' + str(_percentile)
-        else:
-            _percentile = '상위 ' + str(_percentile)
-        print(_avg, _err, _warn, _closestRank, _percentile)
-        return f'[MMR]\n{_avg}±{_err}\n\n{_closestRank}의 {_percentile}%의 소환사들과 비슷합니다.', f'{_avg}±{_err}'
-    except:
-        return '잠시 후 다시시도 해주세요.\n문제가 계속 된다면 문의 바랍니다.', None
+    with ThreadPoolExecutor() as executor:
+        return executor.submit(get_summoner_data, search, 'ARAM').result()
